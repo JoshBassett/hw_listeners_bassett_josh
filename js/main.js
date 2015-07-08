1cfbdf6c94714af4eb2860848c6f8e8f94eb251b
Josh Bassett <jbassett9@gmail.com>
2015-07-07 21:08:27 -0400

    Add h1 id and click eventlistenter -not finished

---
+++ js/main.js
@@ -0,0 +1,11 @@
+document.getElementById("h1Click").addEventListener("click", clickMe);
+
+
+function clickMe() {
+    console.log ('This is click number ' + click );
+
+}
+
+var i = 0;
+var click = (i + 1);
+
